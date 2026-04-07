d927c385515129fa674dd37c61bd2927a78b2d99
Amankumar-G <aman.g@incubyte.co>
2026-04-07 11:35:41 +0530

    feat: update Provider component to use Readonly props and include Toaster

--- client/src/components/ui/provider.tsx
+++ client/src/components/ui/provider.tsx
@@ -3,18 +3,20 @@
 import { ChakraProvider, defaultSystem } from "@chakra-ui/react"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { useState } from "react"
+import { Toaster } from "./toaster"
 import {
   ColorModeProvider,
   type ColorModeProviderProps,
 } from "./color-mode"
 
-export function Provider(props: ColorModeProviderProps) {
+export function Provider(props: Readonly<ColorModeProviderProps>) {
   const [queryClient] = useState(() => new QueryClient())
 
   return (
     <QueryClientProvider client={queryClient}>
       <ChakraProvider value={defaultSystem}>
         <ColorModeProvider forcedTheme="light" {...props} />
+        <Toaster />
       </ChakraProvider>
     </QueryClientProvider>
   )
